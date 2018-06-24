a85ea4904712a123afa027b5ba651e63964287a7
velenk <38948738+velenk@users.noreply.github.com>
2018-06-24 14:42:30 +0800

    quicksort

---
+++ 快速排序.py
@@ -0,0 +1,32 @@
+#quick sort
+def main():
+    list0 = get_list()
+    list0 = quicksort(list0)
+    print(list0)
+
+def get_list():
+    while True:
+        msg = input('Input the number(split with ","):')
+        if msg:
+            try:
+                list0 = [int(n) for n in msg.split(',')]
+                return list0
+            except:
+                print('Error')
+        else:
+            exit()
+
+def quicksort(list0):
+    if len(list0) < 2:
+        return list0
+    else:
+        list1 = list2 = []
+        for i in list0[1:]:
+            if i < list0[0]:
+                list1.append(i)
+            else:
+                list2.append(i)
+        list0 = quicksort(list1) + list0[0] + quicksort(list2)
+        return list0
+
+main()
